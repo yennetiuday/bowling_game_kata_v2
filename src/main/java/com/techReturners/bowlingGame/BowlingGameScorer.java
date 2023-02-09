fe215a019418fc3d03f777ee1e43d6e28e7eccb7
UDAYBHASKAR\yenne <yenneti.uday@hotmail.com>
2023-02-09 11:42:10 +0000

    Added test case for Strike and changed code accordingly

--- src/main/java/com/techReturners/bowlingGame/BowlingGameScorer.java
+++ src/main/java/com/techReturners/bowlingGame/BowlingGameScorer.java
@@ -5,10 +5,14 @@ public class BowlingGameScorer {
 	private int totalScore;
 	
 	private static final String SCORE_ZERO = "-";
+	private static final String SCORE_STRIKE = "X";
 
 	public int scoreCalculator(String score) {
 		if(score.equals(SCORE_ZERO))
-			totalScore = 0;
+			totalScore += 0;
+		else if (score.contains(SCORE_STRIKE)) {
+			totalScore += 10;
+		}
 		
 		return totalScore;
 	}
--- src/test/java/com/techReturners/bowlingGame/BowlingGameScorerTest.java
+++ src/test/java/com/techReturners/bowlingGame/BowlingGameScorerTest.java
@@ -7,9 +7,15 @@ import org.junit.jupiter.api.Test;
 public class BowlingGameScorerTest {
 	
 	@Test
-	public void TestScoreContainsZero(){
+	public void TestScoreZero(){
 		BowlingGameScorer bowlingGameScorer = new BowlingGameScorer();
 		assertEquals(0, bowlingGameScorer.scoreCalculator("-"));
 	}
+	
+	@Test
+	public void TestScoreStrike(){
+		BowlingGameScorer bowlingGameScorer = new BowlingGameScorer();
+		assertEquals(10, bowlingGameScorer.scoreCalculator("X"));
+	}	
 
 }
