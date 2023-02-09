98c483ae889a1c405aac4e6825e266a52ad2fbba
UDAYBHASKAR\yenne <yenneti.uday@hotmail.com>
2023-02-09 17:32:05 +0000

    Added test case for first frame spare and corresponf frame first roll score 3 and corrected code accordingly also refactored both files

--- src/main/java/com/techReturners/bowlingGame/BowlingGameScorer.java
+++ src/main/java/com/techReturners/bowlingGame/BowlingGameScorer.java
@@ -1,31 +1,68 @@
 package com.techReturners.bowlingGame;
 
 public class BowlingGameScorer {
+	
+	private int roll;
+	private int[] rolls = new int[21]; 
 			
 	private static final String ZERO = "-";
 	private static final String STRIKE = "X";
 	private static final String SPARE = "/";
 	
 	public int scoreCalculator(String scoreBoard) {
+		scoreBoardConverter(scoreBoard);
 		int score = 0;
-		String[] frames = scoreBoard.split(" ");
-		for (int frame =0; frame<frames.length; frame++) {
-			if(frameScoreZero(frames[frame])) {
-				score += 0;
-			} else if (frames[frame].startsWith(STRIKE)) {
-				score += 10;
-			} else if (frames[frame].endsWith(SPARE)) {
-				score += 10;
+		int currentRoll = 0;
+		for(int currentFrame = 0; currentFrame<10; currentFrame++) {
+			if (isSpare(currentRoll)) {
+				score += getSpareScore(currentRoll);
+				currentRoll += 2;
 			} else {
-				String[] rolls = frames[frame].split("");
-				score += Integer.valueOf(rolls[0]) + Integer.valueOf(rolls[1]);
+				score += getStandardScore(currentRoll);
+				currentRoll += 2;
 			}
 		}
 		return score;
 	}
+	
+	private void scoreBoardConverter(String scoreBoard) {
+		String[] frames = scoreBoard.split(" ");
+		for (int frame =0; frame<frames.length; frame++) {
+			if(frameScoreZero(frames[frame])) {
+				rolls[roll++] = 0;
+				rolls[roll++] = 0;
+			} else if (frames[frame].contains("-")) {
+				String[] frameRolls = frames[frame].split("");
+				rolls[roll++] = frameRolls[0].equals(ZERO)? 0: Integer.valueOf(frameRolls[0]);
+				rolls[roll++] = frameRolls[1].equals(ZERO)? 0: Integer.valueOf(frameRolls[1]);
+			} else if (frames[frame].startsWith(STRIKE)) {
+				rolls[roll++] = 10;
+			} else if (frames[frame].endsWith(SPARE)) {
+				String[] frameRolls = frames[frame].split("");
+				rolls[roll++] = Integer.valueOf(frameRolls[0]);
+				rolls[roll++] = 10 - Integer.valueOf(frameRolls[0]);
+			} else {
+				String[] frameRolls = frames[frame].split("");
+				rolls[roll++] = Integer.valueOf(frameRolls[0]);
+				rolls[roll++] = Integer.valueOf(frameRolls[1]);
+			}
+		}
+	}
 
 	private boolean frameScoreZero(String frameScore) {
 		return frameScore.startsWith(ZERO) && frameScore.endsWith(ZERO);
+	}
+	
+	private boolean isSpare(int currentRoll) {
+		return getStandardScore(currentRoll) == 10;
+	}
+
+	private int getSpareScore(int currentRoll) {
+		return 10 + rolls[currentRoll+2];
+	}
+
+	private int getStandardScore(int currentRoll) {
+		return rolls[currentRoll] + rolls[currentRoll+1];
 	}
 
 }
--- src/test/java/com/techReturners/bowlingGame/BowlingGameScorerTest.java
+++ src/test/java/com/techReturners/bowlingGame/BowlingGameScorerTest.java
@@ -2,50 +2,56 @@ package com.techReturners.bowlingGame;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BowlingGameScorerTest {
 	
+	private BowlingGameScorer bowlingGameScorer;
+	
+	@BeforeEach
+	public void setUp() {
+		bowlingGameScorer = new BowlingGameScorer();
+	}
+	
 	@Test
-	public void TestScoreZero(){
-		BowlingGameScorer bowlingGameScorer = new BowlingGameScorer();
+	public void testScoreZero(){
 		assertEquals(0, bowlingGameScorer.scoreCalculator("-"));
 	}
 	
 	@Test
-	public void TestScoreStrike(){
-		BowlingGameScorer bowlingGameScorer = new BowlingGameScorer();
+	public void testScoreStrike(){
 		assertEquals(10, bowlingGameScorer.scoreCalculator("X"));
 	}
 	
 	@Test
-	public void TestScoreSpare(){
-		BowlingGameScorer bowlingGameScorer = new BowlingGameScorer();
+	public void testScoreSpare(){
 		assertEquals(10, bowlingGameScorer.scoreCalculator("5/"));
 	}
 	
 	@Test
-	public void TestFrameScoreZero(){
-		BowlingGameScorer bowlingGameScorer = new BowlingGameScorer();
+	public void testFrameScoreZero(){
 		assertEquals(0, bowlingGameScorer.scoreCalculator("--"));
 	}
 	
 	@Test
-	public void TestAllFramesScoreZero(){
-		BowlingGameScorer bowlingGameScorer = new BowlingGameScorer();
+	public void testAllFramesScoreZero(){
 		assertEquals(0, bowlingGameScorer.scoreCalculator("-- -- -- -- -- -- -- -- -- --" ));
 	}
 	
 	@Test
-	public void TestAllFramesScoreOnes(){
-		BowlingGameScorer bowlingGameScorer = new BowlingGameScorer();
+	public void testAllFramesScoreOnes(){
 		assertEquals(20, bowlingGameScorer.scoreCalculator("11 11 11 11 11 11 11 11 11 11" ));
 	}
 	
 	@Test
-	public void TestFramesHavingDifferentScoresWithNoZerosStrikesAndSpares(){
-		BowlingGameScorer bowlingGameScorer = new BowlingGameScorer();
+	public void testFramesHavingDifferentScoresWithNoZerosStrikesAndSpares(){
 		assertEquals(75, bowlingGameScorer.scoreCalculator("21 35 53 45 81 24 54 43 62 17" ));
+	}
+	
+	@Test
+	public void testFramesHavingFirstFrameSpareAndSecondFrameFirstValueFour(){
+		assertEquals(16, bowlingGameScorer.scoreCalculator("4/ 3- -- -- -- -- -- -- -- --" ));
 	}
 
 }
